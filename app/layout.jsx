87cf324ec108947d57e2ab5c2e7b861f9ed6baf3
Deepanshu Chaudhary <chaudharydeepanshu319@gmail.com>
2025-05-06 23:12:31 +0530

    toggle remove

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+// import { ThemeProvider } from "@/components/theme-provider"
+
 import { Toaster } from "@/components/ui/toaster"
 import { AuthProvider } from "@/lib/auth"
 import { NetworkStatus } from "@/components/network-status"
