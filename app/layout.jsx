e06fb439a37624ce19cb9ab2b3929cf2c47563e6
Deepanshu Chaudhary <chaudharydeepanshu319@gmail.com>
2025-05-06 23:28:35 +0530

    toggle code edit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
-// import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider"
 
 import { Toaster } from "@/components/ui/toaster"
 import { AuthProvider } from "@/lib/auth"
